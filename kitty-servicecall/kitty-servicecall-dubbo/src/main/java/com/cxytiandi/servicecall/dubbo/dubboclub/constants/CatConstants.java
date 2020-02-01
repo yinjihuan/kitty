bd4f30917e0660739537da57e3f16505a0bdc025
jihuan.yin@ipiaoniu.com <jihuan.yin@ipiaoniu.com>
2020-02-01 15:15:43 +0800

    dubbo增加调用cat监控

---
+++ kitty-servicecall/kitty-servicecall-dubbo/src/main/java/com/cxytiandi/servicecall/dubbo/dubboclub/constants/CatConstants.java
@@ -0,0 +1,23 @@
+package com.cxytiandi.servicecall.dubbo.dubboclub.constants;
+/**
+ * 代码来自官方给出的示列：https://github.com/dianping/cat/tree/master/integration/dubbo
+ */
+public class CatConstants {
+
+    public static final String CROSS_CONSUMER ="PigeonCall";
+
+    public static final String CROSS_SERVER = "PigeonService";
+
+    public static final String PROVIDER_APPLICATION_NAME="serverApplicationName";
+
+    public static final String CONSUMER_CALL_SERVER="PigeonCall.server";
+
+    public static final String CONSUMER_CALL_APP="PigeonCall.app";
+
+    public static final String CONSUMER_CALL_PORT="PigeonCall.port";
+
+    public static final String PROVIDER_CALL_SERVER="PigeonService.client";
+
+    public static final String PROVIDER_CALL_APP="PigeonService.app";
+
+}
