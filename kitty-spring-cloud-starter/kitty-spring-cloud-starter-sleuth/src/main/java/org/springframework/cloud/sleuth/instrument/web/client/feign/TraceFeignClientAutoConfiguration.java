92f955c8787d0067537fb3f2304b004bbdaabaa1
jihuan.yin@ipiaoniu.com <jihuan.yin@ipiaoniu.com>
2020-03-28 21:23:42 +0800

    sleuth sentinel bugfix

--- kitty-spring-cloud-starter/kitty-spring-cloud-starter-sleuth/src/main/java/org/springframework/cloud/sleuth/instrument/web/client/feign/TraceFeignClientAutoConfiguration.java
+++ kitty-spring-cloud-starter/kitty-spring-cloud-starter-sleuth/src/main/java/org/springframework/cloud/sleuth/instrument/web/client/feign/TraceFeignClientAutoConfiguration.java
@@ -48,7 +48,7 @@ import org.springframework.context.annotation.Scope;
  */
 @Configuration
 @ConditionalOnProperty(value = "spring.sleuth.feign.enabled", matchIfMissing = true)
-@ConditionalOnClass({ Client.class, FeignContext.class })
+@ConditionalOnClass({ Client.class, FeignContext.class, SentinelFeignAutoConfiguration.class })
 @ConditionalOnBean(HttpTracing.class)
 @AutoConfigureBefore({ FeignAutoConfiguration.class, SentinelFeignAutoConfiguration.class })
 @AutoConfigureAfter({ SleuthHystrixAutoConfiguration.class,
