449473c36b55fcc25aeeea81c1ab7b71a7326561
jihuan.yin@ipiaoniu.com <jihuan.yin@ipiaoniu.com>
2020-04-26 22:18:14 +0800

    ES客户端封装优化

--- kitty-db/kitty-db-elasticsearch/src/main/java/com/cxytiandi/kitty/db/elasticsearch/config/ElasticsearchProperties.java
+++ kitty-db/kitty-db-elasticsearch/src/main/java/com/cxytiandi/kitty/db/elasticsearch/config/ElasticsearchProperties.java
@@ -5,6 +5,8 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 /**
+ * ES 配置信息
+ *
  * @作者 尹吉欢
  * @个人微信 jihuan900
  * @微信公众号 猿天地
@@ -26,5 +28,15 @@ public class ElasticsearchProperties {
      * ES端口
      */
     private int port;
+
+    /**
+     * 用户名
+     */
+    private String username;
+
+    /**
+     * 密码
+     */
+    private String password;
 
 }
---
+++ kitty-db/kitty-db-elasticsearch/src/main/java/com/cxytiandi/kitty/db/elasticsearch/constant/ElasticSearchConstant.java
@@ -0,0 +1,31 @@
+package com.cxytiandi.kitty.db.elasticsearch.constant;
+
+/**
+ * @作者 尹吉欢
+ * @个人微信 jihuan900
+ * @微信公众号 猿天地
+ * @GitHub https://github.com/yinjihuan
+ * @作者介绍 http://cxytiandi.com/about
+ * @时间 2020-04-24 22:24
+ */
+public class ElasticSearchConstant {
+
+    public static final String ES_CAT_TYPE = "ElasticSearch";
+
+    public static final String SEARCH = "search";
+
+    public static final String UPDATE = "update";
+
+    public static final String DELETE = "delete";
+
+    public static final String INDEX = "index";
+
+    public static final String UPDATE_REQUEST = "updateRequest";
+
+    public static final String SEARCH_REQUEST = "searchRequest";
+
+    public static final String DELETE_REQUEST = "deleteRequest";
+
+    public static final String INDEX_REQUEST = "indexRequest";
+
+}
