2ad19bf8509a0bbf9396f21b264ddd1288eb459f
jihuan.yin@ipiaoniu.com <jihuan.yin@ipiaoniu.com>
2020-06-21 14:52:50 +0800

    动态线程池扩展支持Apollo配置中心

--- kitty-dynamic-thread-pool/src/main/java/com/cxytiandi/kitty/threadpool/config/DynamicThreadPoolProperties.java
+++ kitty-dynamic-thread-pool/src/main/java/com/cxytiandi/kitty/threadpool/config/DynamicThreadPoolProperties.java
@@ -1,10 +1,18 @@
 package com.cxytiandi.kitty.threadpool.config;
 
 import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.config.YamlPropertiesFactoryBean;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.bind.Bindable;
+import org.springframework.boot.context.properties.bind.Binder;
+import org.springframework.boot.context.properties.source.ConfigurationPropertySource;
+import org.springframework.boot.context.properties.source.MapConfigurationPropertySource;
+import org.springframework.core.io.ByteArrayResource;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.util.*;
 
 /**
  * 动态线程池配置
@@ -16,6 +24,7 @@ import java.util.List;
  * @作者介绍 http://cxytiandi.com/about
  * @时间 2020-05-24 20:36
  */
+@Slf4j
 @Data
 @ConfigurationProperties(prefix = "kitty.threadpools")
 public class DynamicThreadPoolProperties {
@@ -29,6 +38,16 @@ public class DynamicThreadPoolProperties {
      * Nacos Group, 监听配置修改用
      */
     private String nacosGroup;
+
+    /**
+     * Nacos 等待配置刷新时间间隔（监听器收到消息变更通知，此时Spring容器中的配置bean还没更新，需要等待固定的时间）
+     */
+    private int nacosWaitRefreshConfigSeconds = 1;
+
+    /**
+     * Apollo的namespace, 监听配置修改用
+     */
+    private String apolloNamespace;
 
     /**
      * 异常告警API地址，如果配置了此值就将异常信息告诉配置的API,异常告警由外部系统处理
@@ -60,5 +79,34 @@ public class DynamicThreadPoolProperties {
      * 线程池配置
      */
     private List<ThreadPoolProperties> executors = new ArrayList<>();
+
+    /**
+     * 刷新配置
+     * @param content 整个配置文件的内容
+     */
+    public void refresh(String content) {
+        Properties properties =  new Properties();
+        try {
+            properties.load(new ByteArrayInputStream(content.getBytes()));
+        } catch (IOException e) {
+            log.error("转换Properties异常", e);
+        }
+        doRefresh(properties);
+    }
+
+    public void refreshYaml(String content) {
+        YamlPropertiesFactoryBean bean = new YamlPropertiesFactoryBean();
+        bean.setResources(new ByteArrayResource(content.getBytes()));
+        Properties properties = bean.getObject();
+        doRefresh(properties);
+    }
+
+    private void doRefresh(Properties properties) {
+        Map<String, String> dataMap = new HashMap<String, String>((Map) properties);
+        ConfigurationPropertySource sources = new MapConfigurationPropertySource(dataMap);
+
+        Binder binder = new Binder(sources);
+        binder.bind("kitty.threadpools", Bindable.ofInstance(this)).get();
+    }
 
 }
--- kitty-spring-cloud-starter/kitty-spring-cloud-starter-dynamic-thread-pool/src/main/java/com/cxytiandi/kitty/threadpool/autoconfigure/DynamicThreadPoolAutoConfiguration.java
+++ kitty-spring-cloud-starter/kitty-spring-cloud-starter-dynamic-thread-pool/src/main/java/com/cxytiandi/kitty/threadpool/autoconfigure/DynamicThreadPoolAutoConfiguration.java
@@ -4,10 +4,14 @@ import com.cxytiandi.kitty.threadpool.alarm.DynamicThreadPoolAlarm;
 import com.cxytiandi.kitty.threadpool.DynamicThreadPoolManager;
 import com.cxytiandi.kitty.threadpool.config.DynamicThreadPoolProperties;
 import com.cxytiandi.kitty.threadpool.endpoint.ThreadPoolEndpoint;
+import com.cxytiandi.kitty.threadpool.listener.ApolloConfigUpdateListener;
+import com.cxytiandi.kitty.threadpool.listener.NacosConfigUpdateListener;
 import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 
 @ImportAutoConfiguration(DynamicThreadPoolProperties.class)
 @Configuration
@@ -27,6 +31,28 @@ public class DynamicThreadPoolAutoConfiguration {
     @ConditionalOnProperty(value = "kitty.threadpools.alarm.enabled", matchIfMissing = true)
     public DynamicThreadPoolAlarm dynamicThreadPoolAlarm() {
         return new DynamicThreadPoolAlarm();
+    }
+
+    @Configuration
+    @ConditionalOnClass(value = com.alibaba.nacos.api.config.ConfigService.class)
+    protected static class NacosConfiguration {
+
+        @Bean
+        public NacosConfigUpdateListener nacosConfigUpdateListener() {
+            return new NacosConfigUpdateListener();
+        }
+
+    }
+
+    @Configuration
+    @ConditionalOnClass(value = com.ctrip.framework.apollo.ConfigService.class)
+    protected static class ApolloConfiguration {
+
+        @Bean
+        public ApolloConfigUpdateListener apolloConfigUpdateListener() {
+            return new ApolloConfigUpdateListener();
+        }
+
     }
 
 }
