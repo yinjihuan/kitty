950a42ec811f12649f387431836bdc4adaab1311
jihuan.yin@ipiaoniu.com <jihuan.yin@ipiaoniu.com>
2020-07-18 21:17:18 +0800

    支持不需要返回的锁场景

--- kitty-distributed/kitty-distributed-lock/src/main/java/com/cxytiandi/kitty/lock/DistributedLock.java
+++ kitty-distributed/kitty-distributed-lock/src/main/java/com/cxytiandi/kitty/lock/DistributedLock.java
@@ -47,4 +47,36 @@ public interface DistributedLock {
      */
     <T> T lock(String key, int leaseTime, TimeUnit timeUnit, Supplier<T> success, Supplier<T> fail);
 
+    /**
+     * 加锁
+     * @param key 锁Key
+     * @param waitTime 尝试加锁，等待时间 (ms)
+     * @param leaseTime 上锁后的失效时间 (ms)
+     * @param success 锁成功执行的逻辑
+     * @param fail 锁失败执行的逻辑
+     * @return
+     */
+    void lock(String key, int waitTime, int leaseTime, Runnable success, Runnable fail);
+
+    /**
+     * 加锁, 加锁失败立即返回
+     * @param key 锁Key
+     * @param leaseTime 上锁后的失效时间 (ms)
+     * @param success 锁成功执行的逻辑
+     * @param fail 锁失败执行的逻辑
+     * @return
+     */
+    void lock(String key, int leaseTime, Runnable success, Runnable fail);
+
+    /**
+     * 加锁, 加锁失败立即返回
+     * @param key 锁Key
+     * @param leaseTime 上锁后的失效时间
+     * @param timeUnit 时间单位
+     * @param success 锁成功执行的逻辑
+     * @param fail 锁失败执行的逻辑
+     * @return
+     */
+    void lock(String key, int leaseTime, TimeUnit timeUnit, Runnable success, Runnable fail);
+
 }
--- kitty-distributed/kitty-distributed-lock/src/main/java/com/cxytiandi/kitty/lock/DistributedLockRedis.java
+++ kitty-distributed/kitty-distributed-lock/src/main/java/com/cxytiandi/kitty/lock/DistributedLockRedis.java
@@ -45,6 +45,21 @@ public class DistributedLockRedis implements DistributedLock {
         return doLock(key, 0, leaseTime, timeUnit, success, fail);
     }
 
+    @Override
+    public void lock(String key, int waitTime, int leaseTime, Runnable success, Runnable fail) {
+        doLock(key, waitTime, leaseTime, TimeUnit.MILLISECONDS, success, fail);
+    }
+
+    @Override
+    public void lock(String key, int leaseTime, Runnable success, Runnable fail) {
+        doLock(key, 0, leaseTime, TimeUnit.MILLISECONDS, success, fail);
+    }
+
+    @Override
+    public void lock(String key, int leaseTime, TimeUnit timeUnit, Runnable success, Runnable fail) {
+        doLock(key, 0, leaseTime, timeUnit, success, fail);
+    }
+
     private <T> T doLock(String key, int waitTime, int leaseTime, TimeUnit timeUnit, Supplier<T> success, Supplier<T> fail) {
         try {
             RLock lock = null;
@@ -75,6 +90,47 @@ public class DistributedLockRedis implements DistributedLock {
 
             try {
                 return success.get();
+            } catch (Exception e){
+                throw e;
+            } finally {
+                if (lock.getHoldCount() != 0) {
+                    lock.unlock();
+                }
+            }
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void doLock(String key, int waitTime, int leaseTime, TimeUnit timeUnit, Runnable success, Runnable fail) {
+        try {
+            RLock lock = null;
+            try {
+                lock = redissonClient.getLock(key);
+            } catch (Exception e) {
+                log.error("get Redis Lock Error", e);
+                // 降级为数据库锁
+                if (distributedLockMysql != null) {
+                    distributedLockMysql.lock(key, waitTime, leaseTime, success, fail);
+                }
+            }
+
+            boolean tryLock = false;
+            try {
+                tryLock = lock.tryLock(waitTime, leaseTime, timeUnit);
+            } catch (WriteRedisConnectionException e) {
+                // 写入失败，降级为数据库锁
+                if (distributedLockMysql != null) {
+                    distributedLockMysql.lock(key, waitTime, leaseTime, success, fail);
+                }
+            }
+
+            if (!tryLock) {
+                fail.run();
+            }
+
+            try {
+                success.run();
             } catch (Exception e){
                 throw e;
             } finally {
