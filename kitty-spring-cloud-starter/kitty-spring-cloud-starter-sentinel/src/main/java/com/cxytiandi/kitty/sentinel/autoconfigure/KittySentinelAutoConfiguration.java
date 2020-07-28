62183c75ed86007ce4b10945ea67e2ac620b9ed0
jihuan.yin@ipiaoniu.com <jihuan.yin@ipiaoniu.com>
2020-07-28 21:37:05 +0800

    sentinel自动配置

--- kitty-spring-cloud-starter/kitty-spring-cloud-starter-sentinel/src/main/java/com/cxytiandi/kitty/sentinel/autoconfigure/KittySentinelAutoConfiguration.java
+++ kitty-spring-cloud-starter/kitty-spring-cloud-starter-sentinel/src/main/java/com/cxytiandi/kitty/sentinel/autoconfigure/KittySentinelAutoConfiguration.java
@@ -1,11 +1,16 @@
 package com.cxytiandi.kitty.sentinel.autoconfigure;
 
+import com.alibaba.csp.sentinel.adapter.servlet.callback.WebCallbackManager;
+import com.cxytiandi.kitty.sentinel.OriginParserConfig;
 import com.cxytiandi.kitty.sentinel.PathConfig;
 import com.cxytiandi.kitty.sentinel.RestfulUrlCleaner;
+import com.cxytiandi.kitty.sentinel.SentinelRequestOriginParser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.PostConstruct;
 
 /**
  * @作者 尹吉欢
@@ -16,15 +21,22 @@ import org.springframework.context.annotation.Configuration;
  * @时间 2020-07-21 22:07
  */
 @Configuration
-@EnableConfigurationProperties(PathConfig.class)
+@EnableConfigurationProperties({PathConfig.class, OriginParserConfig.class})
 public class KittySentinelAutoConfiguration {
 
     @Autowired
     private PathConfig pathConfig;
+
+    @Autowired
+    private OriginParserConfig originParserConfig;
 
     @Bean
     public RestfulUrlCleaner restfulUrlCleaner() {
         return new RestfulUrlCleaner(pathConfig);
     }
 
+    @PostConstruct
+    public void init() {
+        WebCallbackManager.setRequestOriginParser(new SentinelRequestOriginParser(originParserConfig));
+    }
 }
